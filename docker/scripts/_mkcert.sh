f0773bd730319ce7ef5a359e28d455a15cc7db17
joerivanveen <nonstockphoto@gmail.com>
2024-02-06 09:07:28 +0100

    fix: prevent CERT_DIR is not set error

--- docker/scripts/_mkcert.sh
+++ docker/scripts/_mkcert.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 make-certificates() {
-  if [ ! -f "$CERT_DIR"/"$MAIN_URL".crt ]; then
+  if [ ! -f /etc/mkcert/"$MAIN_URL".crt ]; then
     echo "Make certificates (using mkcert)"
     mkcert -install
-    cd $CERT_DIR
+    cd /etc/mkcert
     # shellcheck disable=SC2086
     mkcert -cert-file "$MAIN_URL".crt -key-file "$MAIN_URL".key *.$MAIN_URL
     openssl dhparam -out ssl-dhparams.pem 2048
